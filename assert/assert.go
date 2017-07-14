3e938cf9e20e6703b0367a9c689d8d55c87f0b18
Marcio Assis <marcio@servicenet.com.br>
2017-07-14 11:24:29 -0300

    mensagem adicional Equals para detalhar a causa

--- assert/assert.go
+++ assert/assert.go
@@ -18,10 +18,13 @@ func Nil(tb testing.TB, err error) {
 }
 
 //Equals equals fails the test if exp is not equal to act.
-func Equals(tb testing.TB, exp, act interface{}) {
+func Equals(tb testing.TB, exp, act interface{}, msgs ...interface{}) {
 	if !reflect.DeepEqual(exp, act) {
 		_, file, line, _ := runtime.Caller(1)
 		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
+		if len(msgs) > 0 {
+			fmt.Printf("\033[31m%s\033[39m\n\n", fmt.Sprintln(msgs...))
+		}
 		tb.FailNow()
 	}
 }
